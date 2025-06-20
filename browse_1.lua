0c8fec7a9ef4d1c97f64388e72033b910310d5e5
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-06-20 18:19:23 +0300

    Create browse_1.lua

---
+++ browse_1.lua
@@ -0,0 +1,23 @@
+[Plugin] = {
+  [Author]: "ddusst"
+  [Note]: "this message not will be show to anyone"
+  [Tabs] = {
+    [Sentences] = { -- Tab Title
+          [Disclaimler]: " "
+          [1] = "im the first phrase"    
+          [2] = "second!"    
+          [3] = "third!"    
+          [4] = "fourth!"    
+          [5] = "better late then never, 5. place"   
+    [Germany] = { -- Tab Title
+          [Disclaimler]: " " 
+          [1] = "no rulez for adolf" 
+          [2] = "heil no!"    
+          [3] = "idk what to put"    
+          [4] = "germany best"    
+          [5] = "your phrase here"    
+      }
+    }
+    [Public] = true
+  }
+}
