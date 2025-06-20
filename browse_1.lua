abb42618b2631e96d8ef0bbf7a5cea772b348d1e
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-06-20 21:49:13 +0300

    Update browse_1.lua

--- browse_1.lua
+++ browse_1.lua
@@ -1,23 +1,27 @@
 [Plugin] = {
-  [Author]: "ddusst"
-  [Note]: "this message not will be show to anyone"
+  [Author] = "ddusst",
+  [Note] = "this message not will be show to anyone",
   [Tabs] = {
-    [Sentences] = { -- Tab Title
-          [Disclaimler]: " "
-          [1] = "im the first phrase"    
-          [2] = "second!"    
-          [3] = "third!"    
-          [4] = "fourth!"    
-          [5] = "better late then never, 5. place"   
-    [Germany] = { -- Tab Title
-          [Disclaimler]: " " 
-          [1] = "no rulez for adolf" 
-          [2] = "heil no!"    
-          [3] = "idk what to put"    
-          [4] = "germany best"    
-          [5] = "your phrase here"    
-      }
+    [Sentences] = {
+      [Disclaimler] = "",
+      [1] = "im the first phrase",
+      [2] = "second!",
+      [3] = "third!",
+      [UI] > [CreateDivider] 
+      [4] = "fourth!",
+      [UI] > [CreateSection] > "text of section"
+      [5] = "better late then never, 5. place"
+    },
+    [Germany] = {
+      [Disclaimler] = "",
+      [1] = "no rulez for adolf",
+      [2] = "heil no!",
+      [UI] > [CreateSection] > "text of section"
+      [3] = "idk what to put",
+      [4] = "germany best",
+      [UI] > [CreateDivider] 
+      [5] = "your phrase here"
     }
-    [Public] = true
-  }
+  },
+  [Public] = true
 }
